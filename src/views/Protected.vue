ee3ac0217f1d8e9d7fd8a86807f7ad4cc86e3909
Felipe Alexandre <felipealexandre@MacBook-Air-de-Felipe.local>
2024-11-18 09:14:07 -0300

    feat: create protected view

---
+++ src/views/Protected.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+      <h2>Bem-vindo à área protegida!</h2>
+      <p>Esta página só pode ser acessada se você estiver autenticado.</p>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    mounted() {
+      const token = localStorage.getItem('token');
+      if (!token) {
+        this.$router.push('/login'); // Redireciona se o token não for encontrado
+      }
+    }
+  };
+  </script>
+  
